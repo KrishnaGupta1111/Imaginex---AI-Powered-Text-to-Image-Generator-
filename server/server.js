b64a75c27e133b4c108cab0824a3d81c2135cf62
Krishna Gupta <krishnaguptaa44@gmail.com>
2025-07-18 14:07:37 +0530

    fix: update CORS to allow Vercel frontend

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://imaginexx.vercel.app", // Change '*' to your frontend domain in production like "https://yourfrontend.vercel.app"
+    origin:"https://imaginexx.vercel.app", // Change '*' to your frontend domain in production like "https://yourfrontend.vercel.app"
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
